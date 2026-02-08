98cbf93a1ad2d94bd9e6f5109b16e40146e044b8
Sipu Padhiari <sipupadhiari6146@gmail.com>
2026-02-08 23:37:11 +0530

    JPA repository Implementation or extend by CategoryRepository

--- src/main/java/com/EcommerceProject/Repositories/CreateRepository.java
+++ src/main/java/com/EcommerceProject/Repositories/CreateRepository.java
@@ -1,4 +1,8 @@
 package com.EcommerceProject.Repositories;
 
-public interface CreateRepository {
+import com.EcommerceProject.Model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CreateRepository extends JpaRepository<Category,Long> { //(Type oF Entity, Type of primary key)
 }
