7f52c5aedda7bb1401693a5181b49fe0a4d22f3f
Sipu Padhiari <sipupadhiari6146@gmail.com>
2026-08-01 23:16:53 +0530

    Adding Order and Payment model in Project

---
+++ src/main/java/com/EcommerceProject/Model/OrderItem.java
@@ -0,0 +1,34 @@
+package com.EcommerceProject.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "order_items")
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name ="order_id" )
+    private Order order;
+
+
+    private Integer quantity;
+    private Double discount;
+    private Double orderProductPrice;
+
+
+
+
+}
